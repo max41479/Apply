f67a9a964084fc27faa2558725d8cb1a952e267a
killerpommes <killerpommes88@web.de>
2012-04-27 14:30:44 +0200

    changed encoding, fixed translation

--- root/language/de/mods/apply.php
+++ root/language/de/mods/apply.php
@@ -6,6 +6,7 @@
 * @copyright (c) 2009 bbDkp <http://code.google.com/p/bbdkp/>
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 * @version $Id$
+* @translation various authors, killerpommes
 * 
 */
  
@@ -50,19 +51,19 @@ $lang = array_merge($lang, array(
 /***** Questionnaire ******/
 'APPLY_MENU' => 'Bewerbungen',
 'APPLY_TITLE' => 'Bewerbungs-Formular',
-'APPLY_INFO' => '[size=150]Willkommen und schön, dass du dich entschieden hast bei uns ein neues zu Hause für deinen Charakter zu suchen. 
-Um uns mit deiner Bewerbung zu untersützen, beantworte bitte die unten aufgeführten Fragen. Gib deinen Charakternamen exakt so an, wie er im Armory angezeigt wird.[/size]  ',
+'APPLY_INFO' => '[size=150]Willkommen und schön, dass du dich entschieden hast, bei uns ein neues zu Hause für deinen Charakter zu suchen. 
+Um dich bei uns zu bewerben, beantworte bitte die unten aufgeführten Fragen. Gib deinen Charakternamen exakt.[/size]  ',
 'APPLY_PUBLICQUESTION' => 'Öffentliche Bewerbung ?', 
 'APPLY_REQUIRED'  => 'Du musst alle Pflichtfelder ausfüllen. ', 
 'MANDATORY'	=> 'Pflichtfeld',	
 'APPLY_REALM' => 'Server (leer für ',
-'APPLY_REALM1' => 'Server: ',
-'APPLY_NAME' => ' Character Name: ',
+'APPLY_NAME' => ' Charakter Name: ',
 'APPLY_QUESTION'  => 'Frage ',
 'APPLY_ANSWER'  => 'Antwort ',
+'APPLY_REALM1' => 'Server: ',
 'APPLY_LEVEL'  => 'Level: ',
 'APPLY_CLASS'  => 'Klasse: ',
-'APPLY_RACE'  => 'Race: ',
+'APPLY_RACE'  => 'Rasse: ',
 'APPLY_TALENT'  => 'Talente: ',
 'APPLY_PROFF'  =>  'Berufe: ',
 
@@ -72,49 +73,50 @@ Um uns mit deiner Bewerbung zu untersützen, beantworte bitte die unten aufgefü
 'APPLY_ACP_FORUM_PRI'		=> 'Bewerbungs Forum (privat) ',
 'APPLY_ACP_FORUM_PRI_EXPLAIN'	=> 'Setze die Gruppenberechtigung für das Forum für Gäste/Bewerber auf: <br />"Post"->"Kann neue Beiträge schreiben"->"Ja",<br/> "Kann Forum lesen" -> "Nein" ',
 'APPLY_ACP_FORUM_PREF'		=> 'Nutzer Einstellung (privat oder öffentlich) ',
-'APPLY_ACP_FORUM_PREF_EXPLAIN'		=> 'entscheidet in welches Forum die Bewerbung geschrieben wird.',
-'APPLY_ACP_FORUM_CHOICE' =>  'Erlaube dem Nutzer die private Auswahl ?',
+'APPLY_ACP_FORUM_PREF_EXPLAIN'		=> 'Entscheidet in welches Forum die Bewerbung geschrieben wird.',
+'APPLY_ACP_FORUM_CHOICE' =>  'Erlaube dem Nutzer die Auswahl "Privat"?',
 'APPLY_ACP_FORUM_CHOICE_EXPLAIN' =>  'Wenn deine Gilde keine privaten Bewerbungen erlaubt, setze diese Option auf "Nein"',
 'APPLY_ACP_PUBLIC'			=> 'öffentlich',
 'APPLY_ACP_PRIVATE'			=> 'privat',
 'APPLY_ACP_GUESTPOST' 		=> 'Können Gäste schreiben? :',
-'APPLY_ACP_GUESTPOST_EXPLAIN' 	=> 'Wenn die Option aktiviert ist, vergiss nicht die Option "Aktiviere visuelle Bestätigung für Gast Postings:" auf "Ja" zu setzen.' ,  
+'APPLY_ACP_GUESTPOST_EXPLAIN' 	=> 'Wenn die Option aktiviert ist, vergiss nicht die Option "Aktiviere visuelle Bestätigung für Gast Beiträge:" auf "Ja" zu setzen.' ,  
 
-/***** ACP template settings *****/
+/***** ACP Armory settings *****/
 'APPLY_ACP_TEMPLATESETTING'	=> 'Formular Einstellungen',
 'APPLY_WELCOME'			=> 'Einladungstext',
-'APPLY_WELCOME_EXPLAIN'		=> 'bbcodes werden umgesetzt. ',
+'APPLY_WELCOME_EXPLAIN'		=> 'BBcodes werden unterstützt. ',
 'APPLY_ACP_CHARNAME' 		=> 'Charakter Name',
-'APPLY_ACP_REALM' 		=> 'Realm',
+'APPLY_ACP_REALM' 		=> 'Server',
 'APPLY_ACP_REGION' 			=> 'Region',
 'APPLY_ACP_APPTEMPLATEUPD'	=> 'Aktualisiere Bewerbungsbogen', 
-'ACP_APPLY_WHATGUILD'  		 => 'Wie soll der Bewerber zur Mitgliederdatei zugefügt werden ? ',
-'ACP_APPLY_WHATGUILD_EXPLAIN' => 'Bewerber bekommt der niedrigster Rank falls Addierung zur Gilde.',
-'ACP_APPLY_GNONE'  			 => 'bei Keine',
-'ACP_APPLY_GSEL'  			 => 'bei ausgewähte Gilde',
 
 /***** ACP template settings *****/
 'ACP_APPLY_MANDATORY'  		=> 'erforderlich',
 'ACP_APPLY_HEADER'  		=> 'Kopfzeile',
-'ACP_APPLY_EXPLAIN'  		=> 'Erklärung',
-'ACP_APPLY_CONTENTS'  		=> 'Inhalt selectbox',
+'ACP_APPLY_QUESTION'  		 => 'Frage',
+'ACP_APPLY_CONTENTS'  		=> 'Wähle Optionen',
+'ACP_APPLY_WHATGUILD'  		 => 'Wie soll der Bewerber zur Mitgliederdatei hinzugefügt werden? ',
+'ACP_APPLY_WHATGUILD_EXPLAIN' => 'Bewerber bekommt den niedrigsten Rank falls er zur Gilde hinzugefügt wird.',
+'ACP_APPLY_GNONE'  			 => 'zu Keine hinzufügen',
+'ACP_APPLY_GSEL'  			 => 'zu ausgewählte Gilde hinzufügen',
+
 'ACP_DKP_APPLY_EXPLAIN'  	=> 'Hier kannst du alle Einstellungen zum Bewerbungsformular vornehmen.',
-'APPLY_ACP_APPTEMPLATENEW'  => 'Bewerbungsvorlage neue Frage', 
+'APPLY_ACP_APPTEMPLATENEW'  => 'Bewerbungsvorlage für neue Frage', 
 'APPLY_CHGMAND' 			=> 'Ändere bestehende Fragen hier. ',
 'APPLY_CHGMAND_EXPLAIN' 	=> 'Ändere die Pflichtprüfung, Reihenfolge, Frage und Art der Eingabe. Grenze verschiedene Optionen durch Komma "," ohne Leerzeichen voneinander ab. Die ersten beiden Fragen sind reserviert.',
 'APPLY_ACP_NEWQUESTION' 	=> 'Trage hier neue Fragen ein.',
 'APPLY_ACP_NEWQUESTION_EXPLAIN' => 'Prüfe ob Pflichtfeld, gib die Ordnungszahl, Frage und Eingabeart an. Grenze verschiedene Optionen durch Komma "," ohne Leerzeichen voneinander ab.', 
 'APPLY_ACP_INPUTBOX' 		=> 'Eingabefeld',	
 'APPLY_ACP_TXTBOX' 			=> 'Textbox', 
-'APPLY_ACP_SELECTBOX' 		=> 'Auswahlbox',
-'APPLY_ACP_RADIOBOX' 		=> 'Optionsfeld (radiobutton)',
+'APPLY_ACP_SELECTBOX' 		=> 'Auswahlbox (selectbox)',
+'APPLY_ACP_RADIOBOX' 		=> 'Auswahlknöpfe (radiobutton)',
 'APPLY_ACP_CHECKBOX' 		=> 'Kontrollkästchen (checkbox)',
 
 //warnings
 'APPLY_ACP_RETURN' 		=> '<h3>Zurück zur Bewerbungskonfiguration.</h3>',
 'APPLY_ACP_REALMBLANKWARN' 	=> 'Server Feld darf nicht leer sein.', 
 'APPLY_ACP_SETTINGSAVED' 	=> 'allgemeine Bewerbungseinstellungen gespeichert',
-'APPLY_NO_GUILD'		=> 'Gilde ist erforderlich', 
+'APPLY_NO_GUILD'		=> 'keine Gilde', 
 //upd
 'APPLY_ACP_TWOREALM' 		=> 'Du kannst keine 2 Server oder Charakternamen einrichten.', 
 'APPLY_ACP_QUESTUPD' 		=> 'Bewerbungsfragen aktualisiert',
@@ -122,7 +124,7 @@ Um uns mit deiner Bewerbung zu untersützen, beantworte bitte die unten aufgefü
 'APPLY_ACP_ORDQUEST' 		=> 'Du musst die Reihenfolge, Fragen und Optionen ausfüllen bevor die Frage gespeichert werden darf.',
 'APPLY_ACP_QUESTNOTADD' 	=> 'Fehler : Frage wurde nicht gespeichert !', 
 'APPLY_ACP_QUESTNADD' 		=> 'Neue Frage wurde gespeichert !',   
-'APPLY_ACP_EXPLAINOPTIONS' 	=> 'Einzelne Option begrenazt durch komma "," ohne Leerzeichen.',  
+'APPLY_ACP_EXPLAINOPTIONS' 	=> 'Begrenze einzelne Optionen mit Komma "," ohne Leerzeichen.',  
 
 /** ACP settings for posting template **/
 'APPLY_COLORSETTINGS' 		=> 'Farbinstellungen',
@@ -136,12 +138,12 @@ Um uns mit deiner Bewerbung zu untersützen, beantworte bitte die unten aufgefü
 'APPLY_CHAR_OVERVIEW' 		=> 'Charakter',
 'APPLY_CHAR_MOTIVATION' 	=> 'Motivation',
 
-'APPLY_CHAR_NAME' 	=> '[color=%s][b]Character Name : [/b][/color]%s',
-'APPLY_CHAR_LEVEL' 	=> '[color=%s]Character Level : [/color]%s',  
-'APPLY_CHAR_CLASS' 	=> '[color=%s]Character Klasse : [/color]%s' ,
-'APPLY_CHAR_PROFF' 	=> '[color=%s][u]Berufe :[/u][/color]
+'APPLY_CHAR_NAME' 	=> '[color=%s][b]Charaktername : [/b][/color]%s',
+'APPLY_CHAR_LEVEL' 	=> '[color=%s]Charakterlevel : [/color]%s',  
+'APPLY_CHAR_CLASS' 	=> '[color=%s]Charakterklasse : [/color]%s' ,
+'APPLY_CHAR_PROFF' 	=> '[color=%s][u]Berufe des Charakters:[/u][/color]
 %s',
-'APPLY_CHAR_BUILD' 	=> '[color=%s][u]Talent Verteilungen : [/u][/color]%s',
+'APPLY_CHAR_BUILD' 	=> '[color=%s][u]Talent Verteilung : [/u][/color]%s',
 'APPLY_CHAR_URL' => '[color=%s][/color][url=%s]WoW Armory Link[/url]', 
 'APPLY_ERROR_NAME'  =>  'Fehler : Name muss alphabetisch (a-zA-ZàäåâÅÂçÇéèëêïÏîÎæŒæÆÅóòÓÒöÖôÔøØüÜ sind erlaubt). ',
 'APPLY_REQUIRED_LEVEL'  => 'Level ist erforderlich. ', 
